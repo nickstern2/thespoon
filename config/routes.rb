8cb3bb53d578139a06fc17b43cc5f160446bc018
guido caldara <guidocaldara@gmail.com>
2019-07-30 11:40:58 +0800

    crud completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #CRUD
+  #READ
+  resources :restaurants
+  # get "/restaurants", to: "restaurants#index", as: "restaurants"
+  # get "/restaurants/:id", to: "restaurants#show", as: "restaurant"
+
+  # #CREATE
+  # get "/restaurants/new", to: "restaurants#new", as: "new_restaurant"
+  # post "/restaurants", to: "restaurants#create"
+  # #UPDATE
+  # get "/restaurants/:id/edit", to: "restaurants#edit", as: "edit_restaurant"
+  # patch "/restaurants/:id", to: "restaurants#update"
+  # #DELETE
+  # delete "/restaurants/:id", to: "restaurants#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "Sika", stars: 4, address: "Canggu")
+Restaurant.create(name: "Varune", stars: 2, address: "Canggu")
+Restaurant.create(name: "Deus", stars: 1, address: "Canggu")
